e7e4851ab5a1832667bb3ae2a560ea70a4dd09fc
Daniel Frehner <dfrehner1@gmail.com>
2017-02-18 11:26:11 -0700

    Update index.js

--- client/dev/index.js
+++ client/dev/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore/*, applyMiddleware */} from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './app';
-
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
